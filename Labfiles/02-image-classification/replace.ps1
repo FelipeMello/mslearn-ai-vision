163d3335b7a33d65e2f6fd2fe0d8803727490245
unknown <d23125661@mytudublin.ie>
2024-07-01 23:22:53 +0100

    first commit

--- Labfiles/02-image-classification/replace.ps1
+++ Labfiles/02-image-classification/replace.ps1
@@ -1,2 +1,2 @@
-$storageAcct = '<storageAccount>'
+$storageAcct = 'customclassifystorageac'
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
